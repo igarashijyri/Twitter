a30debbcffe9e1628455e9c716c4756c93b532da
igarashijyri <fourleafclover688@gmail.com>
2023-07-05 23:02:05 +0900

    12.3.3

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,9 +1,10 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+# TODO: デバッグ用の実装であり、本来はユーザに応じて引数を変更する必要がある
 class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/account_activation
   def account_activation
-    # ToDo: デバッグ用の実装であり、本来はユーザに応じて引数を変更する必要がある
+
     user = User.first
     user.activation_token = User.new_token
     UserMailer.account_activation(user)
@@ -11,7 +12,8 @@ class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
-
 end
